6b3dd43017342ebb5a0dbf0279314798d5273a01
JulianReid541 <reidjulian@outlook.com>
2020-08-06 15:36:22 -0700

    updated and changed files

--- react-chat/src/Firebase.js
+++ react-chat/src/Firebase.js
@@ -4,9 +4,9 @@ import firestore from 'firebase/firestore'
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-    projectID = process.env.REACT_APP_PROJECT_ID,
+    projectID: process.env.REACT_APP_PROJECT_ID,
     apiKey: process.env.REACT_APP_APIKEY,
-    databaseURL: REACT_APP_DB_URL
+    databaseURL: 'https://reactchat-28cb5.firebaseio.com'
 };
 
 firebase.initializeApp(config);
